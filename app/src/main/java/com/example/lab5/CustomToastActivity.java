c1a97d1a00d84bc531701205bfe894fc1ad521f6
thomaskao <thomaskao99@gmail.com>
2021-11-05 20:31:45 +0800

    first

---
+++ app/src/main/java/com/example/lab5/CustomToastActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomToastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_toast);
+    }
+}
